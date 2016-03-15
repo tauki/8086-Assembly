c42f67f9cfa7a9a2eb3efc2c0b0d922e278ac1f0
Tauki Tahmid <ttauki@gmail.com>
2016-03-15 16:36:20 +0600

    Update Project.asm

--- Project.asm
+++ Project.asm
@@ -34,8 +34,8 @@ include 'emu8086.inc'
 .stack 100h
 
 .data
-  newline db 13, 10, 0
-  input dw  0
+  userInput_main dw  0
+  userInput_secondary dw 0
 
 .code   
   mov ax,@data
@@ -49,8 +49,8 @@ include 'emu8086.inc'
   DEFINE_PTHIS
   DEFINE_CLEAR_SCREEN
   DEFINE_GET_STRING
-
-
+  
+  
 ;*****************************************************************************************************
 ;* procedure #0 (main)                                                                               *
 ;*                                                                                                   *
@@ -59,9 +59,13 @@ include 'emu8086.inc'
 ;*                                                                                                   *
 ;* Program will exit upon entering -1 when asked                                                     *
 ;*                                                                                                   *
+;* #calling procedure: call main                                                                     *
+;* #Defining procedure: define_main                                                                  *
+;*                                                                                                   *
 ;*****************************************************************************************************
-
+  
   main proc
+   
     printn "Input 1 for Fibonacci"
     printn "Input 2 For HappyNumber"
     printn "Input 3 For PerfectNumber"
@@ -71,28 +75,27 @@ include 'emu8086.inc'
     call scan_num       
     
     ;moving curser to the nextLine
-    mov si, offset newline
-    call print_string
+    call newLine
     
     ;storing the input in "input"
-    mov input, cx
+    mov userInput_main, cx
    
     ;deciding which proc to call (or if)
     cmp cx, -1
     je call End
     
     ;to call Fibonacci
-    mov cx, input
+    mov cx, userInput_main
     cmp cx, 1
     je call Fibonacci
     
     ;to call HappyNumber
-    mov cx, input
+    mov cx, userInput_main
     cmp cx, 2
     je call HappyNumber
     
     ;to call PerfectNumber
-    mov cx, input
+    mov cx, userInput_main
     cmp cx, 3
     je call PerfectNumber 
     
@@ -114,12 +117,17 @@ include 'emu8086.inc'
 ;* procedure #1                                                                                      *  
 ;*                                                                                                   *
 ;* This procedure will prompt for the n'th number                                                    *
-;* ie. if userInput is 10, the program will print the fibonacci series containing 10 numbers         *
+;* ie. if userInput is 10, the program will print the fibonacci series till 10'th numbers            *
+;*                                                                                                   *
+;* #calling procedure: call Fibonacci                                                                *
+;* #Defining procedure: define_Fibonacci                                                             *
 ;*                                                                                                   *
 ;*****************************************************************************************************
 
   Fibonacci proc
     call clear_Screen
+    call Reset_reg
+    
     
     call main 
   endp Fibonacci
@@ -130,6 +138,9 @@ include 'emu8086.inc'
 ;*                                                                                                   *
 ;* The program will prompt for a number to check if the number is HappyNumber                        *
 ;* ie. if userInput is 10, the program will check if 10 is a HappyNumber                             *
+;*                                                                                                   *
+;* #calling procedure: call HappyNumber                                                              *
+;* #Defining procedure: define_HappyNumber                                                           *
 ;*                                                                                                   *
 ;*****************************************************************************************************
 
@@ -145,6 +156,9 @@ include 'emu8086.inc'
 ;*                                                                                                   *
 ;* The program will prompt for a number to check if the number is a PerfectNumber                    *
 ;* ie. if userInput is 10, the program will check if 10 is a PerfectNumber                           *
+;*                                                                                                   *
+;* #calling procedure: call PerfectNumber                                                            *
+;* #Defining procedure: define_PerfectNumber                                                         *
 ;*                                                                                                   *
 ;*****************************************************************************************************
 
@@ -163,6 +177,9 @@ include 'emu8086.inc'
 ;* prompt "Invalid Input"                                                                            *
 ;* And return to main                                                                                *
 ;*                                                                                                   *
+;* #calling procedure: call NotValid                                                                 *
+;* #Defining procedure: define_NotValid                                                              *
+;*                                                                                                   *
 ;*****************************************************************************************************
 
   NotValid proc
@@ -171,6 +188,47 @@ include 'emu8086.inc'
     printn "Invalid Input"
     call main
   endp NotValid
+ 
+  
+;*****************************************************************************************************
+;* Procedure Reset_reg                                                                               *
+;*                                                                                                   *
+;* This procedure will reset the value of each general purpose register                              *
+;* And then, return to the calling procedure                                                         *
+;*                                                                                                   *
+;* #calling procedure: call reset                                                                    *
+;* #Defining procedure: define_reset                                                                 *
+;*                                                                                                   *
+;*****************************************************************************************************
+
+  Reset_reg proc
+    mov ax, 0
+    mov bx, 0
+    mov cx, 0
+    mov dx, 0
+    ret
+  endp Reset_reg
+  
+  
+;*****************************************************************************************************
+;* Procedure Reset_reg                                                                               *
+;*                                                                                                   *
+;* This procedure will move the curser to the next line and to the left-most edge                    *
+;* And then, return to the calling procedure                                                         *
+;*                                                                                                   *
+;* #calling procedure: call newLine                                                                  *
+;* #Defining procedure: define_newLine                                                               *
+;*                                                                                                   *
+;*****************************************************************************************************
+
+  newLine proc
+    mov ah, 2
+    mov dl, 10
+    int 21h
+    mov dl, 13
+    int 21h
+    ret
+  endp newLine
 
 
 ;******************************************************
@@ -178,3 +236,4 @@ include 'emu8086.inc'
 ;*            END OF THE PROGRAM                      *
 ;*                                                    *
 ;******************************************************
+END main
